137c47d4bda3d950337e5f612f3292d038f77d2b
GoldenSecrets <raerae734@gmail.com>
2020-08-10 16:28:37 -0400

    Initial Commit

---
+++ src/PracticeRun2.java
@@ -0,0 +1,12 @@
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PracticeRun2 extends JavaPlugin {
+    public PracticeRun2() {
+    }
+
+    public void onDisable() {
+    }
+
+    public void onEnable() {
+    }
+}
